2e8413096aa625736b67f634c33d96ca9b22a4ed
Yullia90 <y.tsarenko@harwind.com.ua>
2023-07-29 00:35:01 +0300

    add structure

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Welcome to Node.js");
+// CommonJS-module
+// const nodemon = require("nodemon");
+// const users = require("./db/contacts.json");
+
+// es6-type
+// import users from "./db/contacts.json"
